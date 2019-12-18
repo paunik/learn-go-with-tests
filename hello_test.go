670b113c390d6f636f6859aed770bb71967783b0
Nikola <nikola.paunovic@legal.one>
2019-12-18 11:36:25 +0100

    Refactor test, and add multilanguage test

--- hello_test.go
+++ hello_test.go
@@ -4,21 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
 	t.Run("saying hello to named people", func(t *testing.T) {
 		got := Hello("Test")
 		want := "Hello, Test"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Serbian", func(t *testing.T) {
+		got := Hello("Milivoje", "Serbian")
+		want := "Pomaze Bog, Milivoje"
+		assertCorrectMessage(t, got, want)
 	})
 }
