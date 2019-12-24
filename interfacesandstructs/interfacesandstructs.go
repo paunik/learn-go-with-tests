73fec1cd9cf2e67c537b362c4abee3cbd70325e6
Nikola <nikola.paunovic@legal.one>
2019-12-24 21:52:02 +0100

    Refactor functions -> methods

--- interfacesandstructs/interfacesandstructs.go
+++ interfacesandstructs/interfacesandstructs.go
@@ -5,10 +5,18 @@ type Rectangle struct {
 	Width  float64
 }
 
-func Perimeter(r Rectangle) float64 {
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 0.0
+}
+
+func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Height + r.Width)
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
 }
--- interfacesandstructs/interfacesandstructs_test.go
+++ interfacesandstructs/interfacesandstructs_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -15,7 +15,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 100.0
 
 		if got != want {
@@ -25,7 +25,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := Area(circle)
+		got := circle.Area()
 		want := 314.1592653589793
 
 		if got != want {
