a922d06d9f6a0b236673afdf39f6a03066c9ba90
Nikola <nikola.paunovic@legal.one>
2019-12-24 21:59:30 +0100

    Added triangle code

--- interfacesandstructs/interfacesandstructs.go
+++ interfacesandstructs/interfacesandstructs.go
@@ -15,6 +15,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
@@ -25,4 +30,8 @@ func (r Rectangle) Perimeter() float64 {
 
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width
+}
+
+func (t Triangle) Area() float64 {
+	return t.Height * t.Base / 2
 }
