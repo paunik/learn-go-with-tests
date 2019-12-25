94043f6bf501dc988a6a35f39c5075fecbe2289b
Nikola <nikola.paunovic@legal.one>
2019-12-25 09:06:39 +0100

    Pass test basic

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,5 @@
 package main
 
-func Search(dictionary map[string]string, needle string) string {
-	return ""
+func Search(dictionary map[string]string, word string) string {
+	return dictionary[word]
 }
