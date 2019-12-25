1379e6b79cee118afc08762650679bf865307923
Nikola <nikola.paunovic@legal.one>
2019-12-25 09:09:44 +0100

    Add custom type, improve test readability

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,10 +3,15 @@ package main
 import "testing"
 
 func TestDictionary(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
 	got := Search(dictionary, "test")
 	want := "this is just a test"
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q, want %q given %q", got, want, "test")
