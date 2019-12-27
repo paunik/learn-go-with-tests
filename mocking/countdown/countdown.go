e0f9c792248a4fee84fd32ac02643b6184841a2a
Nikola <nikola.paunovic@legal.one>
2019-12-27 16:27:25 +0100

    Mocking

---
+++ mocking/countdown/countdown.go
@@ -0,0 +1,29 @@
+package countdown
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprintf(out, finalWord)
+}
