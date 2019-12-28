ee603ef05a50fe99414d1ec1656ea160facc1254
Nikola <nikola.paunovic@legal.one>
2019-12-28 08:38:33 +0100

    Select used in concurrency

--- select/select.go
+++ select/select.go
@@ -1,5 +1,35 @@
 package selectexample
 
-func Racer(url1, url2 string) (winner string) {
-	return
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var tenSecondTimeout = 10 * time.Second
+
+func Racer(urlOne, urlTwo string) (winner string, err error) {
+	return ConfigurableRacer(urlOne, urlTwo, tenSecondTimeout)
+}
+
+func ConfigurableRacer(urlOne, urlTwo string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(urlOne):
+		return urlOne, nil
+	case <-ping(urlTwo):
+		return urlTwo, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", urlOne, urlTwo)
+	}
+
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
--- select/select_test.go
+++ select/select_test.go
@@ -1,15 +1,51 @@
 package selectexample
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+	t.Run("runs regular test where both are responsive", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("should err if 10 seconds elapsed with no response", func(t *testing.T) {
+
+		slowServer := makeDelayedServer(25 * time.Millisecond)
+		defer slowServer.Close()
+
+		slowURL := slowServer.URL
+
+		_, err := ConfigurableRacer(slowURL, slowURL, time.Millisecond*20)
+
+		if err == nil {
+			t.Errorf("expected error but didn't get one")
+		}
+	})
+
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
