a0adb27c14ba554b59a0d29223a42d28aa383137
Nikola <nikola.paunovic@legal.one>
2019-12-31 22:52:23 +0100

    Add docs to methods

--- context/context.go
+++ context/context.go
@@ -4,10 +4,12 @@ import "net/http"
 
 import "fmt"
 
+// Store interface
 type Store interface {
 	Fetch() string
 }
 
+// Server provides http.HandlerFunc
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Print(w, store.Fetch())
