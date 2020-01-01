89e989a0a9d9249caaea6ce5f5ea67c024e50c42
Nikola <nikola.paunovic@legal.one>
2020-01-01 10:35:07 +0100

    Support for Number 1000

--- romannumerals/romannumerals.go
+++ romannumerals/romannumerals.go
@@ -2,19 +2,35 @@ package romannumerals
 
 import "strings"
 
+// RomanNumeral represents roman numeral and arabic value
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var RomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+// ConvertToRoman converts arabic digit into Roman numeral
 func ConvertToRoman(a int) string {
 	var result strings.Builder
-	for a > 0 {
-		switch {
-		case a > 4:
-			result.WriteString("V")
-			a -= 5
-		case a > 3:
-			result.WriteString("IV")
-			a -= 4
-		default:
-			result.WriteString("I")
-			a--
+	for _, numeral := range RomanNumerals {
+		for a >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			a -= numeral.Value
 		}
 	}
 
